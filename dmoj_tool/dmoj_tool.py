f7084daeb12021b042432330475f248531382fbb
Dessertion <73731354pi@gmail.com>
2020-04-04 18:12:24 -0400

    it worksgit add .

--- dmoj_tool/dmoj_tool.py
+++ dmoj_tool/dmoj_tool.py
@@ -5,26 +5,17 @@ import sys
 import time
 from urllib.error import HTTPError
 
-def main(problem,language,path_to_code):
+def main(token,problem,language,path_to_code):
     # print(problem,path_to_code)
     
     base_url = 'https://dmoj.ca'
     
-    try:
-        f = open('../token','r+')
-    except FileNotFoundError:
-        print("Token file does not exist! Please ensure that your token is in a file called 'token'.")
-        open('../token','w+')
-        sys.exit(1)
     
     try:
         code = open(path_to_code).read()
     except Exception:
         raise 
     
-    api = requests.get(f'{base_url}/api/problem/list')
-    
-    token = f.readline()
     headers = {'Authorization':f'Bearer {token}'}
     doc = requests.get(base_url+'/user',headers=headers)
     try:
@@ -57,14 +48,16 @@ def main(problem,language,path_to_code):
         submission = requests.get(f'{base_url}/widgets/single_submission?id={submission_num}',headers=headers)
         
         soup1 = BeautifulSoup(submission.text,'html.parser')
+        print(soup1.find(class_='status')['title'],end='')
         
-        if soup1.find(class_='status').text != 'G':
+        if all(x not in soup1.find(class_='status').text for x in ['G','P']):
+            print(':\n')
             testcases = requests.get(f'{base_url}/widgets/submission_testcases?id={submission_num}',headers=headers)
             soup2 = BeautifulSoup(testcases.text,'html.parser')
             cases = soup2.find_all('table')
             for case in cases:
                 if case.findPrevious('b'):
-                    print(case.findPrevious('b'))
+                    print(case.findPrevious('b').text)
                 batch = list(case.find_all('tr')) 
                 for row in batch:
                     print(' '.join([' '.join(x.text.split()) if type(x)==Tag else ''.join(x.split()) for x in row]))
@@ -74,7 +67,8 @@ def main(problem,language,path_to_code):
             print(f'Time: {soup1.find(class_="sub-usage").find(class_="time").text.split()[-1]}')
             
             break
-                
+        print('...')
+        
         time.sleep(1)
         
     
@@ -84,4 +78,11 @@ if __name__ == '__main__':
         print("Helpful help message.")
     else:
         # submit to problem
-        main(sys.argv[1],sys.argv[2],sys.argv[3])
+        try:
+            f = open('../token','r+')
+        except FileNotFoundError:
+            print("Token file does not exist! Please ensure that your token is configured with `configure` or `-c`")
+            open('../token','w+')
+            sys.exit(1)
+        
+        main(f.read(),sys.argv[1],sys.argv[2],sys.argv[3])
--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ import setuptools
 
 setuptools.setup(
     name="dmoj-tool-dessertion",
-    version="0.0.3",
+    version="0.0.9",
     author="Desertion",
     author_email="73731354pi@gmail.com",
     description="CLI submission to DMOJ",
+    scripts=['bin/dmoj-tool'],
     packages=setuptools.find_packages(),
     install_requires=['beautifulsoup4','bs4','certifi','chardet','idna','requests','soupsieve','urllib3'],
     python_requires='>=3.6'
